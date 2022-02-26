ef89bfa0fe57fa71aab47c6f829f7c60fa40114b
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2022-02-26 11:29:25 +0530

    Added truffle to the project

---
+++ truffle/truffle-config.js
@@ -0,0 +1,33 @@
+const Kit = require("@celo/contractkit");
+const kit = Kit.newKit("https://alfajores-forno.celo-testnet.org");
+const getAccount = require("./getAccount").getAccount;
+
+async function awaitWrapper() {
+  let account = await getAccount();
+  kit.connection.addAccount(account.privateKey);
+}
+awaitWrapper();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+    },
+    alfajores: {
+      provider: kit.connection.web3.currentProvider,
+      network_id: 44787,
+    },
+  },
+
+  compilers: {
+    solc: {
+      version: "0.8.2",
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
