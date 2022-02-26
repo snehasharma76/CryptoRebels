790d0a435cdb68b884c44143d9ee98ab7cb96a01
Nandan-unni <asnqln@gmail.com>
2022-02-26 22:41:45 +0530

    profile not responsive

--- frontend/src/components/Common/Button.js
+++ frontend/src/components/Common/Button.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
 
-const Button = ({ text, url }) => {
+const Button = ({ text, url, className }) => {
   return (
     <Link href={url}>
-      <a className="bg-white text-black px-[64px] font-semibold py-[16px] rounded-md">
+      <a
+        className={`bg-white text-black px-[64px] font-semibold py-[16px] rounded-md ${className}`}
+      >
         {text}
       </a>
     </Link>
---
+++ frontend/src/components/Common/ProfilePostCard.js
@@ -0,0 +1,21 @@
+const ProfilePostCard = () => {
+  const postImg =
+    "https://www.datocms-assets.com/46272/1633199491-1633199490440.jpg";
+  return (
+    <figure className="my-8">
+      <img src={postImg} alt="post img" className="w-[500px]" />
+      <figcaption className="w-[500px]">
+        <h2 className="text-xl font-semibold mt-4 mb-2">
+          Lorem ipsum dolor lorem ipsum lorem dolor
+        </h2>
+        <p className="text-sm text-neutral-200">
+          Lorem ipsum dolor lorem ipsum lorem dolor. Lorem ipsum dolor lorem
+          ipsum lorem dolor. Lorem ipsum dolor lorem ipsum lorem dolor. Lorem
+          ipsum dolor lorem ipsum lorem dolor.
+        </p>
+      </figcaption>
+    </figure>
+  );
+};
+
+export default ProfilePostCard;
